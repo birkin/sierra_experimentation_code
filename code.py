41330d2c7ce44a425a3d357551beac4d09bfdbd3
birkin <birkin.diana@gmail.com>
2018-04-05 11:45:39 -0400

    starts token work.

--- code.py
+++ code.py
@@ -18,12 +18,21 @@ log.debug( '\n-------\nstarting standard log' )
 if (sys.version_info < (3, 0)):
     raise Exception( 'forcing myself to use python3 always' )
 
-
+API_ROOT_URL = os.environ['SAE__ROOT_URL']
 HTTPBASIC_KEY = os.environ['SAE__HTTPBASIC_USERNAME']
 HTTPBASIC_SECRET = os.environ['SAE__HTTPBASIC_PASSWORD']
 
 ## ok, let's get to work! ##
 
-r = requests.get( url, auth=HTTPBasicAuth(HTTPBASIC_KEY, HTTPBASIC_SECRET) )
+## get the token
 
-log.debug( 'r.content, ```%s```' % r.content )
+url = '%stoken' % API_ROOT_URL
+log.debug( 'url for token, ```%s```' % url )
+
+
+
+
+
+# r = requests.get( API_ROOT_URL, auth=HTTPBasicAuth(HTTPBASIC_KEY, HTTPBASIC_SECRET) )
+
+# log.debug( 'r.content, ```%s```' % r.content )
