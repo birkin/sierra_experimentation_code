0e88fe96287b3745bdac902b6e5778f56b8e0e90
birkin <birkin.diana@gmail.com>
2018-04-05 13:14:29 -0400

    grabs end-bib.

--- code.py
+++ code.py
@@ -2,7 +2,7 @@
 SAE__ prefix for "Sierra API Experiementation"
 '''
 
-import logging, os, sys
+import datetime, logging, os, sys
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -33,17 +33,31 @@ log.debug( 'token r.content, ```%s```' % r.content )
 token = r.json()['access_token']
 log.debug( 'token, ```%s```' % token )
 
-## make a bib-request
+## make a bib-request, just as a test
 
-bib_url = '%sbibs/?id=1000001' % API_ROOT_URL
+# bib_url = '%sbibs/?id=1000001' % API_ROOT_URL
+bib_url = '%sbibs/' % API_ROOT_URL
+payload = { 'id': '1000001' }
 log.debug( 'token_url, ```%s```' % token_url )
 custom_headers = {'Authorization': 'Bearer %s' % token }
-r = requests.get( bib_url, headers=custom_headers )
+r = requests.get( bib_url, headers=custom_headers, params=payload )
 log.debug( 'bib r.content, ```%s```' % r.content )
 
+## ok, we have the first bib, let's get the last (hack, close to the last)
+"""
+TODO thought... there's probably a way to query the api to get this value.
+A hack, though, would be to run a cron job that would just get all the bibs over the last x/hours,
+and save the last one to a file at a specified location that the code below would load.
+"""
+log.debug( '\n-------\ngetting end-bib\n-------' )
+today_date = str( datetime.date.today() )
+start_datetime = '%sT00:00:00Z' % today_date
+end_datetime = '%sT23:59:59Z' % today_date
+payload = {
+    'limit': '1', 'createdDate': '[%s,%s]' % (start_datetime, end_datetime)  }
+r = requests.get( bib_url, headers=custom_headers, params=payload )
+log.debug( 'bib r.content, ```%s```' % r.content )
+end_bib = r.json()['entries'][0]['id']
+log.debug( 'end_bib, `%s`' % end_bib )
 
 
-
-# r = requests.get( API_ROOT_URL, auth=HTTPBasicAuth(HTTPBASIC_KEY, HTTPBASIC_SECRET) )
-
-# log.debug( 'r.content, ```%s```' % r.content )
