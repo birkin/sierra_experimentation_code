4124bb5aac5ded363990f9b407e314f824ef0cde
birkin <birkin.diana@gmail.com>
2018-04-05 10:26:44 -0400

    adds documentation info and github-code info.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-code to experiment with iii's sierra.
+III Sierra API experimentation...
+
+- Resources
+
+    - Documentation
+
+            From H.C.:
+
+            Start here: https://techdocs.iii.com/sierraapi/Content/zReference/introduction.htm
+
+            Then here: https://techdocs.iii.com/sierraapi/Content/zReference/operations.htm
+
+            And finally here: https://sandbox.iii.com/iii/sierra-api/swagger/index.html
+            (this is where you can run test API calls against their sandbox, but it has great documentation on the input and output of each endpoint)
+
+    - Code
+
+        - [github search](https://github.com/search?p=1&q=iii+sierra&type=Repositories&utf8=✓)
+            - [filtered for python](https://github.com/search?l=Python&q=iii+sierra&type=Repositories)
+
+---
+
--- code.py
+++ code.py
@@ -1,14 +1,24 @@
-import os
+import logging, os
 import requests
 from requests.auth import HTTPBasicAuth
 
 
-GRIZZLY_HTTPBASIC_USER = os.environ['GRIZZLY_HTTPBASIC_USER']
-GRIZZLY_HTTPBASIC_PASSWORD = os.environ['GRIZZLY_HTTPBASIC_PASSWORD']
+logging.basicConfig(
+    # filename=settings.SOME_PATH,
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S'
+    )
+log = logging.getLogger(__name__)
+log.debug( '\n-------\nstarting standard log' )
 
 
-log.debug( 'user, `%s`' % GRIZZLY_HTTPBASIC_USER )
+GRIZZLY_HTTPBASIC_KEY = os.environ['GRIZZLY_HTTPBASIC_KEY']
+GRIZZLY_HTTPBASIC_SECRET = os.environ['GRIZZLY_HTTPBASIC_SECRET']
 
-r = requests.get( url, auth=HTTPBasicAuth(GRIZZLY_HTTPBASIC_USER, GRIZZLY_HTTPBASIC_PASSWORD) )
+
+log.debug( 'user, `%s`' % GRIZZLY_HTTPBASIC_KEY )
+
+r = requests.get( url, auth=HTTPBasicAuth(GRIZZLY_HTTPBASIC_KEY, GRIZZLY_HTTPBASIC_SECRET) )
 
 log.debug( 'r.content, ```%s```' % r.content )
