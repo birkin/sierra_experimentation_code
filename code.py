a5ad28a909f80fc3489a8007829350550e56fa5c
birkin <birkin.diana@gmail.com>
2018-04-05 10:27:05 -0400

    cleans up setup.

--- code.py
+++ code.py
@@ -1,10 +1,13 @@
-import logging, os
+'''
+SAE__ prefix for "Sierra API Experiementation"
+'''
+
+import logging, os, sys
 import requests
 from requests.auth import HTTPBasicAuth
 
-
 logging.basicConfig(
-    # filename=settings.SOME_PATH,
+    # filename=os.environ['SAE__LOG_PATH'],
     level=logging.DEBUG,
     format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
     datefmt='%d/%b/%Y %H:%M:%S'
@@ -12,13 +15,15 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 log.debug( '\n-------\nstarting standard log' )
 
+if (sys.version_info < (3, 0)):
+    raise Exception( 'forcing myself to use python3 always' )
 
-GRIZZLY_HTTPBASIC_KEY = os.environ['GRIZZLY_HTTPBASIC_KEY']
-GRIZZLY_HTTPBASIC_SECRET = os.environ['GRIZZLY_HTTPBASIC_SECRET']
 
+HTTPBASIC_KEY = os.environ['SAE__HTTPBASIC_USERNAME']
+HTTPBASIC_SECRET = os.environ['SAE__HTTPBASIC_PASSWORD']
 
-log.debug( 'user, `%s`' % GRIZZLY_HTTPBASIC_KEY )
+## ok, let's get to work! ##
 
-r = requests.get( url, auth=HTTPBasicAuth(GRIZZLY_HTTPBASIC_KEY, GRIZZLY_HTTPBASIC_SECRET) )
+r = requests.get( url, auth=HTTPBasicAuth(HTTPBASIC_KEY, HTTPBASIC_SECRET) )
 
 log.debug( 'r.content, ```%s```' % r.content )
